52ec12b683bfc835f2101d5828805590591f6bdd
Skh121 <sabinkhadka121@gmail.com>
2023-12-28 23:36:43 +0545

    Day:2 Leetcode sum of good pairs

---
+++ NumOfGoodPair.java
@@ -0,0 +1,15 @@
+class NumberOfGoodPair {
+    public int numIdenticalPairs(int[] nums) {
+        int sumOfGoodPairs =0;
+        for(int i=0;i<nums.length;i++){
+            for(int j =0;j<nums.length;j++){
+                if(nums[i]==nums[j] && i<j){
+                    sumOfGoodPairs+=1;
+                }
+            }
+        }
+        return sumOfGoodPairs;
+        }
+}
+
+
