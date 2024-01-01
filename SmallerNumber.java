1908ca12e04cee3735405e3d93367dc9ab14d7a7
Skh121 <sabinkhadka121@gmail.com>
2024-01-01 22:02:22 +0545

    Day:6 Leetcode Smaller Numbers Than Current Number

---
+++ SmallerNumber.java
@@ -0,0 +1,16 @@
+public class SmallerNumber {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int count =0;
+        int[] arr = new int[nums.length];
+        for(int i =0;i<nums.length;i++){
+            for(int j =0;j<nums.length;j++){
+                if (i!=j && nums[i]>nums[j]){
+                        count +=1;
+                }
+            }
+            arr[i]=count;
+            count =0;
+        }
+        return arr;
+    }
+}
