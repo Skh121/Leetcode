7c2c8d7eaa5c915815d6cfa4c2d25ff51dd830a2
Skh121 <sabinkhadka121@gmail.com>
2024-01-02 22:38:09 +0545

    Day:7 Leetcode Running Sum

---
+++ RunningSum.java
@@ -0,0 +1,14 @@
+public class RunningSum {
+    public int[] runningSum(int[] nums) {
+        int sum =0;
+        int [] arr = new int[nums.length];
+        for (int i =0;i<nums.length;i++){
+            for (int j =0;j<=i;j++){
+                sum = sum + nums[j];
+            }
+            arr[i] = sum;
+            sum =0;
+        }
+        return arr;
+    }
+}
